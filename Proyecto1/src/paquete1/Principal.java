f3abdd857c29a4437b5772ea19d3eca32769d138
Diruno <Diruno@DESKTOP-61SITF8>
2024-01-12 11:41:57 +0200

    Creacion de la clase proyecto1

---
+++ Proyecto1/src/paquete1/Principal.java
@@ -0,0 +1,8 @@
+package paquete1;
+
+public class Principal {
+	public static void main(String[] args) {
+		//TDO auto-generated method stub
+		System.out.println("Ejemplo de control de versiones con Git");
+	}
+}
